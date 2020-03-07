be33e3c92378817422e8a902ce7b2d62c506b454
casinesque <marcocasini21@gmail.com>
2020-03-07 19:25:18 +0100

    aggiunta similarità tra testi cn Spacy

--- Caller.py
+++ Caller.py
@@ -3,7 +3,14 @@ import pprint
 
 artist='The Beatles'
 #song="Yellow SubMarine"
+#GetMixedLyrics.find_similarity()
+
+#sim= GetMixedLyrics.calculate_similarity()
 
 #list= GetMixedLyrics.get_songs_from_artist_lyrics(artist)
 list = GetMixedLyrics.get_all_lyrics_from_an_artist(artist)
 print(list)
+print(list[0])
+print(list[1])
+sim= GetMixedLyrics.calculate_similarity(list[0],list[1])
+print(sim)
