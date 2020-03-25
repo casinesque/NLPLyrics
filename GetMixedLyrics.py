f13c6cd7b2ab44d4116bad482afefa32521a61fa
casinesque <marcocasini21@gmail.com>
2020-03-25 22:56:26 +0100

    Improved general overall

--- GetMixedLyrics.py
+++ GetMixedLyrics.py
@@ -32,6 +32,8 @@ proxies = {
 
 MINIMUM_EDIT_DISTANCE_THRESHOLD = 1
 
+MAX_SIMILARITY_TRESHOLD = 0.80
+
 nlp = en_core_web_md.load()
 '''
 def remove_invalid_chars(input_string):
@@ -49,8 +51,8 @@ def remove_invalid_chars(input_string):
 def process_text(text):
     doc = nlp(text.lower())
     result = []
-    for token in doc:  # 'come and get it' e 'come together' venivano ridotte a 'come' e basta! NO!
-        # if token.text in nlp.Defaults.stop_words: #mi porta via troppe parole. Provo a commentarlo.
+    for token in doc:  # e.g. 'come and get it' and 'come together' were reduced to just 'come'. Not enough information. Commented.
+        # if token.text in nlp.Defaults.stop_words:
         #    continue
         if token.is_punct:
             continue
@@ -81,13 +83,11 @@ def remove_parenthesis(text):
 
 
 def leaveOnlyAlphabeticalChars(text):
-    # [^a-zA-Z]
     clean = re.compile('[,\.!\?-]')
     return re.sub(clean, '', str(text))
 
 
 def removeSpaces(text):
-    # [^a-zA-Z]
     clean = re.compile('[^a-zA-Z]')
     return re.sub(clean, '', str(text))
 
@@ -97,31 +97,28 @@ def remove_duplicates_by_dict(words):
     unique = []
     mapNameUrl = dict(zip(words[::2], words[1::2]))
     unique = mapNameUrl.items()
-    unique = [[i, j] for i, j in unique]  # trasformo lista di tuple in lista
+    unique = [[i, j] for i, j in unique]  # Trasforming list of tuples in a list
     flattened_unique_list = []
     list_of_similar = []
     for sublist in unique:  # list flattening !
         for element in sublist:
             flattened_unique_list.append(element)
-            # CONTROLLO LA MED. SOLO CON TRESHOLD A 1 SONO SICURO DI NON TOGLIERE CANZONI BUONE.
     for left, right in zip(flattened_unique_list[:-2], flattened_unique_list[2:]):
-        # Pensare se metterla##################
-        #if calculate_similarity(left, right) > 0.95 or editdistance.eval(left,right) <= MINIMUM_EDIT_DISTANCE_THRESHOLD or removeSpaces(right) in removeSpaces(left) or removeSpaces(left) in removeSpaces(right):  # rimuovo tutte quelle sporche o che hanno nomi allungati della stessa canzone
+        #if calculate_similarity(left, right) > 0.95 or editdistance.eval(left,right) <= MINIMUM_EDIT_DISTANCE_THRESHOLD or removeSpaces(right) in removeSpaces(left) or removeSpaces(left) in removeSpaces(right):  # REMOVING ALL THE NOISE NAMES OR WITH NON STANDARD NAMES, OR REPLICATED NAMES
             if not left.startswith("https:"):
-                if SequenceMatcher(None, left, right).ratio() > 0.80:
-            #eval = editdistance.eval(left, right)
-            #print(calculate_similarity(left, right))
-
+                if SequenceMatcher(None, left, right).ratio() > MAX_SIMILARITY_TRESHOLD:
                     longer = left if len(left) > len(right) else right
-                    list_of_similar.append(longer)  # empiricamente i dx sono quelli piu corretti, quindi cancello quelli a sx.
+                    '''Empirically those on the right side are the more correct so i'm deleting the left values'''
+                    list_of_similar.append(longer)
     for key in list(mapNameUrl.keys()):
         if key in list_of_similar:
             try:
                 del mapNameUrl[key]
             except KeyError:
                 print("Key  not found")
+
     cleanedList = mapNameUrl.items()
-    unique = [[i, j] for i, j in cleanedList]  # ritorno la lista in uscita pulita !
+    unique = [[i, j] for i, j in cleanedList]  # returning the clean list
     return unique
 
 
@@ -148,25 +145,27 @@ def get_songs_from_artist_lyrics(artist):
             name = name.lower()
             name = re.sub(r'\[.*?\]', "", name)  # RIMUOVO PARENTESI [
             name = re.sub(r'\(.*?\)', "", name)  # RIMUOVO PARENTESI (
+            name = re.sub(r'\{.*?\}', "", name)  # RIMUOVO PARENTESI (
             name = name.split('[')[0]  # RIMUOVO PARENTESI (
             name = name.split('(')[0]  # RIMUOVO PARENTESI (
             name = name.strip()
-            name= name.replace("’","")
             name= name.replace("‘", '').replace("’", '').replace("'", '')
+            name= name.replace('"', '').replace('/"',"")
             name= remove_invalid_chars(name)
             if name not in finalNameList:
-                url = (str(htmlRowUrl[0])).split('"')[1]  # ricavo link per ogni canzone
+                url = (str(htmlRowUrl[0])).split('"')[1]  # gathering url for each correct song
                 url = "https://www.lyrics.com" + url
                 finalNameList.append(leaveOnlyAlphabeticalChars(name))
-                # ORA HO UNA LISTA COI NOMI UGUALI A CUI HO RIMOSSO COMPLETAMENTE LA PUNTEGGIATURA. UTILE PER CHIAMARCI UN DICT. SOPRA E RIMUOVERE DUPLICATI.
                 finalUrlList.append(url)
         finalList = [val for pair in zip(finalNameList, finalUrlList) for val in pair]
         all_separated_final_list = [list(x) for x in
                                     zip(finalList[::2], finalList[1::2])]  # Ho creato una lista di liste [[[]]
-        finalList = remove_duplicates_by_dict(
-            finalList)  # ECCO QUA CHE HO CREATO IL DIZIONARIO SENZA USARE LA FUNZIONE APPOSITA!
-        print(finalList)
-        return finalList
+        #Now we have a whole list with some equals name. We call a method which works using dict in order to automatically remove duplicates
+        finalList = remove_duplicates_by_dict(finalList)
+        if finalList:
+            return finalList
+        else:
+            raise Exception('No results for this artist. Please check your input and retry.')
     except Exception as e:
         return "Exception occurred \n" + str(e)
 
@@ -174,64 +173,18 @@ def get_songs_from_artist_lyrics(artist):
 def get_all_lyrics_from_an_artist(artist):
     artist = artist.lower()
     all_songs = get_songs_from_artist_lyrics(artist)
-    # Lista dove ogni elemento è [nome canzone, url] -- CONTIENE DUPLICATI E SIMILTUDINI.
-    # all_separated_songs=[list(x) for x in zip(all_songs[::2], all_songs[1::2])] # creo una lista di sottoliste [[nome,url]...]
     list_of_words = []
+    #At this point i have a list with [[name,url][name,url]]. We simply gather each lyrics from each song simply parsing the url associated.
     for item in all_songs:
         url_lyric = (item[1])
-        # time.sleep(0.5)
+        # time.sleep(0.5) OPTIONAL: If fastness is not a priority and we query lots of data. This slow down the request made to the lyrics.com
         content = requests.get(url_lyric).text
         only_pre_tag = SoupStrainer('pre', {
-            'id': 'lyric-body-text'})  # Permette di scaricare solo quella classe non tutto la pagina web!
+            'id': 'lyric-body-text'})  # Allows to parse just this css id instead of the whole webpage. 1.5x faster
         soup = BeautifulSoup(content, 'lxml', parse_only=only_pre_tag)
-        noHtmlCleanedText = remove_html_tags(str(soup))  # rimuovo html dal content
-        cleaned_text = remove_parenthesis(noHtmlCleanedText)  # rimuovo parentesi dai testi
-        print("Downloading lyrics of:" + remove_parenthesis(item[0]))
+        noHtmlCleanedText = remove_html_tags(str(soup))  # removing all html tags
+        cleaned_text = remove_parenthesis(noHtmlCleanedText)  # removing noises characters
+        print("Downloading lyric of:" + remove_parenthesis(item[0]))
         list_of_words.append(cleaned_text.replace('\n', ' ').replace('\r', ''))
-    return list_of_words  # lista di tutte le parole.
+    return list_of_words  # Huge list with all the words in it.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def get_lyric_by_artist(artist, song_title):
-    artist = artist.lower()
-    song_title = song_title.lower()
-    # remove all except alphanumeric characters from artist and song_title
-    artist = re.sub('[^A-Za-z0-9]+', "", artist)
-    song_title = re.sub('[^A-Za-z0-9]+', "", song_title)
-    if artist.startswith("the"):  # remove starting 'the' from artist e.g. the who -> who
-        artist = artist[3:]
-    url = "https: // www.metrolyrics.com /" + artist + "-" + "lyrics" + ".html"
-
-    try:
-        content = urllib.request.urlopen(url).read()
-        soup = BeautifulSoup(content, 'html.parser')
-        lyrics = str(soup)
-        # lyrics lies between up_partition and down_partition
-        up_partition = '<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->'
-        down_partition = '<!-- MxM banner -->'
-        lyrics = lyrics.split(up_partition)[1]
-        lyrics = lyrics.split(down_partition)[0]
-        lyrics = remove_html_tags(lyrics)
-        return lyrics
-    except Exception as e:
-        return "Exception occurred \n" + str(e)
